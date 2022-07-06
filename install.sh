8347b975a031a541a0a10715acdc13cec0eb9336
Siddhant Khare <siddhant@gitpod.io>
2022-07-06 10:59:42 +0000

    install deno

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get install -y
 echo "This Statement is using Gitpod Dotfiles Feature 🧡🙋‍♂️"
+curl -fsSL https://deno.land/x/install/install.sh | sh
+export DENO_INSTALL="/home/gitpod/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
