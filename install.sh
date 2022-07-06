ac8c7bbc48957646a39bcdfce1eb44b61f231f6e
Siddhant Khare <siddhant@gitpod.io>
2022-07-06 21:52:26 +0530

    Install & Configure Deno

--- install.sh
+++ install.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
-sudo apt-get install -y
-echo "This Statement is using Gitpod Dotfiles Feature 🧡🙋‍♂️"
+
+# Install & Configure Deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
 target="$HOME/.deno";
 
 printf 'export %s\n' "DENO_INSTALL=$target"\
                                 "PATH=$target/bin:\$PATH" >> $HOME/.bashrc.d/600-deno
-
-
-
-# echo $DENO_INSTALL
-# echo "source ${DOTFILES_DIR}/bash/bashrc.bash" >> ~/.bashrc
-# echo "${DENO_INSTALL}" >> ~/.bashrc
-# echo "${PATH}" >> ~/.bashrc
-
-# echo "${DENO_INSTALL}"
-
-
-# nix-shell -p deno
