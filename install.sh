9234d8583ffc71c6c5e4ff53f9b548380c572c98
Siddhant Khare <siddhant@gitpod.io>
2022-07-06 11:23:35 +0000

    exec

--- install.sh
+++ install.sh
@@ -4,10 +4,15 @@ echo "This Statement is using Gitpod Dotfiles Feature 🧡🙋‍♂️"
 curl -fsSL https://deno.land/x/install/install.sh | sh
 export DENO_INSTALL="/home/gitpod/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
+
+
+
 # echo $DENO_INSTALL
 # echo "source ${DOTFILES_DIR}/bash/bashrc.bash" >> ~/.bashrc
-echo "${DENO_INSTALL}" >> ~/.bashrc
-echo "${PATH}" >> ~/.bashrc
+# echo "${DENO_INSTALL}" >> ~/.bashrc
+# echo "${PATH}" >> ~/.bashrc
+
+# echo "${DENO_INSTALL}"
 
 
 # nix-shell -p deno
